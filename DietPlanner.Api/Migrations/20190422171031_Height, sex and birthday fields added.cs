af2fc81da467e5e423738f7567f8652609b3dbc6
KptBalkon <arkadiuszozieblo@gmail.com>
2019-04-22 19:13:12 +0200

    added height, sex and birthday fields for user, added plan calculator but it's not complete

---
+++ DietPlanner.Api/Migrations/20190422171031_Height, sex and birthday fields added.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DietPlanner.Api.Migrations
+{
+    public partial class Heightsexandbirthdayfieldsadded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Birthday",
+                table: "Users",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<int>(
+                name: "Height",
+                table: "Users",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Sex",
+                table: "Users",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Birthday",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Height",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Sex",
+                table: "Users");
+        }
+    }
+}
--- DietPlanner.Core/Domain/User.cs
+++ DietPlanner.Core/Domain/User.cs
@@ -18,6 +18,10 @@ namespace DietPlanner.Core.Domain
         public string Role { get; protected set; }
         public DateTime CreatedAt { get; protected set; }
         public DateTime UpdatedAt { get; protected set; }
+        public int Height { get; protected set; }
+        public string Sex { get; protected set; }
+        public DateTime Birthday { get; protected set; }
+        public int Age => GetAge();
 
         public ISet<WeightPoint> WeightPoints { get; set; }
 
@@ -37,6 +41,50 @@ namespace DietPlanner.Core.Domain
             CreatedAt = DateTime.UtcNow;
             UpdatedAt = DateTime.UtcNow;
             SetRole(role);
+        }
+
+        public void SetStats(int height, string sex, DateTime birthday)
+        {
+            SetHeight(height);
+            SetSex(sex);
+            SetBirthday(birthday);
+        }
+
+        private void SetBirthday(DateTime birthday)
+        {
+            //Expected: result1 >=0
+            int result1 = DateTime.Compare(birthday, DateTime.UtcNow.AddYears(-150));
+            //Expected: result2 <=0
+            int result2 = DateTime.Compare(birthday, DateTime.UtcNow.AddYears(-12));
+            if (result1 >=0 && result2 <=0)
+            {
+                Birthday = birthday;
+            }
+            return;
+        }
+
+        private void SetSex(string sex)
+        {
+            if (sex.ToLowerInvariant() == "male")
+            {
+                Sex = "Male";
+                return;
+            }
+            if (sex.ToLowerInvariant() == "female")
+            {
+                Sex = "Female";
+                return;
+            }
+            Sex = "Unknown";
+        }
+
+        private void SetHeight(int height)
+        {
+            if (height<50 || height > 300)
+            {
+                throw new Exception("User's height must be between 50 and 300 centimeters");
+            }
+            Height = height;
         }
 
         protected void SetPassword(string password)
@@ -92,17 +140,25 @@ namespace DietPlanner.Core.Domain
         public void CreatePlan(int plannedWeight, DateTime targetDate)
         {
             Plan = Plan.Create(UserId, plannedWeight, targetDate);
-    }
+        }
+
+        public int GetAge()
+        {
+            return (DateTime.Now - Birthday).Days;
+        }
 
         public static User Create(Guid userId, string username, string email, string role, string password, string salt)
             => new User(userId, username, email, role, password, salt);
 
-        public void Update(string username, string email, string role, string password, string salt)
+        public void Update(string username, string email, string role, string password, string salt, string height, string sex, DateTime birthday)
         {
             if (username!=null) this.SetUsername(username);
             if (email!=null) this.SetEmail(email);
             if (role!=null) this.SetRole(role);
             if (password != null) this.SetPassword(password);
+            if (height != null) this.SetHeight(Int32.Parse(height));
+            if (sex != null) this.SetSex(sex);
+            if (birthday != null) this.SetBirthday(birthday);
             Salt = salt;
             UpdatedAt = DateTime.UtcNow;
         }
